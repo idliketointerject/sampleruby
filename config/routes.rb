9850ccbbfa850e948e0805c982db3603232fccc4
Alexander Smith <alexander_smith@umail.ucsb.edu>
2014-02-07 10:32:58 -0800

    sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SampleApp::Application.routes.draw do
   resources :users
-  root 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
+  root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
